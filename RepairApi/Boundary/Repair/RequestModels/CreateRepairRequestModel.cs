96c0da92e66c4636a780abc6205fc0f189293745
Egor Dubrovskiy <egor.dubrovskiy@liveswitch.com>
2022-05-28 21:38:34 +0300

    make master id nullable

--- RepairApi/Boundary/Repair/RequestModels/CreateRepairRequestModel.cs
+++ RepairApi/Boundary/Repair/RequestModels/CreateRepairRequestModel.cs
@@ -9,7 +9,8 @@ namespace RepairApi.Boundary.Repair.RequestModels
         public DateTime Date { get; set; }
         public string AdvancedInfo { get; set; }
         public string Status { get; set; }
-        public Guid MasterId { get; set; }
+        public Guid? MasterId { get; set; }
         public Guid ClientId { get; set; }
+        public Guid ProductId { get; set; }
     }
 }
